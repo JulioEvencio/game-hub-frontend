fadf7ce5f983c31a7a5ab9ae68719d27db4b5b74
JulioEvencio <julioigreja.dev@gmail.com>
2024-10-03 17:12:18 -0300

    added: footer

---
+++ game-hub/src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div>
+                <p>
+                    Developed by <strong>Júlio Igreja</strong> - 2024
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
