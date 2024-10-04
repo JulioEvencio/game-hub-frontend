81455819d5bd58527e8027a32ee22f3298c3a8df
JulioEvencio <julioigreja.dev@gmail.com>
2024-10-03 23:32:47 -0300

    update: footer

--- game-hub/src/components/footer/Footer.jsx
+++ game-hub/src/components/footer/Footer.jsx
@@ -1,11 +1,13 @@
 import styles from './Footer.module.css'
 
+import { Link } from 'react-router-dom'
+
 function Footer() {
     return (
         <footer className={styles.footer}>
             <div>
                 <p>
-                    Developed by <strong>Júlio Igreja</strong> - 2024
+                    Developed by <Link to='https://github.com/JulioEvencio' target='_blank'><strong>Júlio Igreja</strong></Link>
                 </p>
             </div>
         </footer>
