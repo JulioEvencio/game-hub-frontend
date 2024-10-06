6a373c35589a046e1fff82d27efc8f59a75691c6
JulioEvencio <julioigreja.dev@gmail.com>
2024-10-06 00:11:46 -0300

    added: profile page with data mock

---
+++ game-hub/src/components/game/GameComponent.jsx
@@ -0,0 +1,19 @@
+import styles from './GameComponent.module.css'
+
+import { Link } from 'react-router-dom'
+
+function GameComponent({ username, name, slug, src }) {
+    return (
+        <div className={styles.game}>
+            <Link to={`/${username}-game/${slug}`}>
+                <figure>
+                    <img src={src} alt='Game' />
+                </figure>
+
+                <h3>{name}</h3>
+            </Link>
+        </div>
+    )
+}
+
+export default GameComponent
--- game-hub/src/pages/profile/Profile.jsx
+++ game-hub/src/pages/profile/Profile.jsx
@@ -1,12 +1,77 @@
-import { useAuthContext } from '../../hooks/useAuthContext'
+import styles from './Profile.module.css'
+
+import { useNavigate } from 'react-router-dom'
+
+import GameComponent from '../../components/game/GameComponent'
 
 function Profile() {
-    const { authContext } = useAuthContext()
-    
+    const games = [
+        {
+            username: 'username',
+            name: 'Silmance',
+            picture: 'https://img.itch.zone/aW1nLzE2MDM3OTM3LnBuZw==/315x250%23c/ZQl0Is.png',
+            slug: 'silmance'
+        },
+        {
+            username: 'username',
+            name: 'Control Pixel',
+            picture: 'https://img.itch.zone/aW1nLzE1MTYwNDUzLnBuZw==/315x250%23c/BVdcyx.png',
+            slug: 'control-pixel'
+        },
+        {
+            username: 'username',
+            name: 'Dyrvania',
+            picture: 'https://img.itch.zone/aW1nLzE1NDE3MzcwLnBuZw==/315x250%23c/vG4cIJ.png',
+            slug: 'dyrvania'
+        }
+    ]
+
+    const navigate = useNavigate()
+
+    const handlePublishGame = () => {
+        // Code
+    }
+
+    const handleSettings = () => {
+        // Code
+    }
+
+    const handleLogout = () => {
+        navigate('/logout')
+    }
+
     return (
-        <div>
-            <h1>Profile</h1>
-            <p>{authContext.accessToken}</p>
+        <div className={styles.profile}>
+            <section className={styles.user}>
+                <figure>
+                    <img src={`https://api.dicebear.com/9.x/bottts/svg?seed=${games[0].user}`} alt='Profile'></img>
+                </figure>
+
+                <h1>Username</h1>
+
+                <button onClick={handlePublishGame}>Publish game</button>
+                <button onClick={handleSettings}>Settings</button>
+                <button onClick={handleLogout}>Logout</button>
+            </section>
+
+            <section className={styles.card}>
+                <h2>My Games</h2>
+                <hr />
+
+                <div>
+                    {
+                        games.map((game, i) => (
+                            <GameComponent
+                                key={i}
+                                username={game.username}
+                                name={game.name}
+                                slug={game.slug}
+                                src={game.picture}
+                            />
+                        ))
+                    }
+                </div>
+            </section>
         </div>
     )
 }
